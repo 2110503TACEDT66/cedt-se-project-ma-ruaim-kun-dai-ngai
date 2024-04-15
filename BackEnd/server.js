de0e0a812ea8dbb388867a85bb6ce3fea8c99ce9
Paimee <145831785+Paimeews@users.noreply.github.com>
2024-04-16 01:10:53 +0700

    Backend for review

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -18,18 +18,16 @@ app.use(cookieParser());
 
 
 const campgrounds = require('./routes/campgrounds');
-
 const users = require('./routes/auth')
 const bookings=require('./routes/bookings');
+const reviews = require('./routes/reviews')
 
 app.use('/api/v1/campgrounds',campgrounds);
-
 app.use('/api/v1/auth',users);
-
 app.use('/api/v1/bookings',bookings);
+app.use('/api/v1/reviews',reviews);
 
 const PORT = process.env.PORT || 5000;
-
 const server =  app.listen( PORT, console.log('Server running in ' ,process.env.NODE_ENV, ' mode on port ', PORT));
 
 process.on('unhandledRejection' , (err,promise)=>{
