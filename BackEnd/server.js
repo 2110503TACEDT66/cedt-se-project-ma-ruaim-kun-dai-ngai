7c426dca62ca5df5f745f0965c516d605ee67f26
ravisaradx <145831809+ravisaradx@users.noreply.github.com>
2024-04-29 13:21:48 +0700

    add swagger auth, bookings, campgrounds

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -8,12 +8,34 @@ const {xss} = require('express-xss-sanitizer');
 const rateLimit = require('express-rate-limit');
 const hpp = require('hpp');
 const cors = require('cors');
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUI = require('swagger-ui-express');
 
 dotenv.config({path:'./config/config.env'});
 
 connectDB();
 
 const app = express();
+
+const swaggerOptions = {
+    swaggerDefinition:{
+        openapi: '3.0.0',
+        info:{
+            title: 'Library API',
+            version: '1.0.0',
+            description: 'A simple Express Campground API'
+        },
+        servers: [
+            {
+                url: 'http://localhost:5000/api/v1'
+            }
+        ],
+    },
+    apis:['./routes/*.js'],
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
 //body parser
 app.use(express.json());
@@ -41,7 +63,8 @@ const campgrounds = require('./routes/campgrounds');
 const users = require('./routes/auth')
 const bookings=require('./routes/bookings');
 const reviews = require('./routes/reviews')
-const replys = require('./routes/replys')
+const replys = require('./routes/replys');
+const swaggerJSDoc = require('swagger-jsdoc');
 // const rates = require('./routes/rates')
 
 app.use('/api/v1/campgrounds',campgrounds);
