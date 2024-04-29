a3e1f2407de3eba72ffc1882ef1a54d9fa00890b
Paimee <145831785+Paimeews@users.noreply.github.com>
2024-04-29 18:28:07 +0700

    edit error

--- BackEnd/routes/campgrounds.js
+++ BackEnd/routes/campgrounds.js
@@ -194,7 +194,7 @@
   router.use('/:campgroundId/rates/',ratesRouter)
 
   router.route('/').get(getCampgrounds).post(protect,authorize('admin'),createCampground);
-  router.route('/:id').get(getCampground).put(protect,authorize('admin'),updateCampground).delete(protect,authorize('admin'),deleteCampground);
+  router.route('/:id').get(getCampground).put(updateCampground).delete(protect,authorize('admin'),deleteCampground);
 
   
 
